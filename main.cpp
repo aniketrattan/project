6345f649617910bfbb4a2e9be06a9769e6690855
OliverLuscombe <161267631+OliverLuscombe@users.noreply.github.com>
2024-05-18 22:29:48 +0930

    Delete main.cpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include "GameGraphic.h"
-#include "Definitions.h"
-int main(){
-    graphics::GameGraphic(SCREEN_WIDTH, SCREEN_HEIGHT, "SFML Test");
-
-    return 0;
-}
