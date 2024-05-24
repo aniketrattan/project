f1d0fc26ad8694609a2f517c20cb20fd4dbd5fc7
Arej00 <161259945+Arej00@users.noreply.github.com>
2024-05-24 14:22:00 +0930

    Delete Items.cpp

--- Items.cpp
+++
@@ -1,8 +0,0 @@
-#include "Items.h"
-
-// constructor
-Items::Items(string name, int cost) : name(name), cost(cost) {}
-
-// getters
-string Items::get_name() const { return name; }
-int Items::get_cost() const { return cost; }
