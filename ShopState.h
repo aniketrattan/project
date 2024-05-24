b51401e89df6fa59909f5c11e1be301104fd9ec0
OliverLuscombe <161267631+OliverLuscombe@users.noreply.github.com>
2024-05-24 14:31:28 +0930

    Add files via upload

---
+++ ShopState.h
@@ -0,0 +1,70 @@
+#ifndef shopstate_h
+#define shopstate_h
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include "Definitions.h"
+#include "GameGraphic.h"
+#include "MenuDisplay.h"
+#include "DescriptionDisplay.h"
+#include "state.h"
+#include "ShopUpper.h"
+#include "EncounterState.h"
+#include "Shop.h"
+
+namespace graphics {
+
+class ShopState : public State {
+ private:
+  GameDataRef _data;
+  // sprites
+  sf::Sprite _background;
+  vector<int> _bought;
+  int _boughtItems = 0;
+  //menu items
+  MenuDisplay* _menuItems;
+  ShopUpper* _menuUpper;
+  DescriptionDisplay* _ItemDescription;
+  DescriptionDisplay* _ItemDescriptionLast;
+
+  //shop setup
+  Shop shop;
+
+  //need to work
+  Fighter* c1 = new Fighter(100);
+  Wizard* c2 = new Wizard(100);
+  Cleric* c3 = new Cleric(100);
+
+  //money
+  int money = c1->get_money();
+  // what values will be shown from the paths vector
+  int menuBeginning;
+  int menuEnd;
+
+  vector<string> items = {"1.Reapers blade 200g", "2.Fire Ring 200g", "3.Holy Water 200g", 
+  "4.Life essence 400g", "5.Action boost 400g", "6.Second chance 400g"};
+
+  vector<string> descriptions = {"adds more damage and \nbleed to Severing Slash","increases damage for fireball","increases damage \nmitagation from protection",
+  "increase max health \nof all characters", "adds 1 more action point \nper round", 
+  "when a character would drop \nto 0 health they instead go\nto 50 health. This item \ncan only be used once."};
+
+  vector<string> upper = {"Gold: ", "up) encounter"};
+
+  vector<int> fileItems = {7,7,7,7,7,7};
+
+ public:
+  ShopState(GameDataRef data);
+
+  void Init();
+
+  void handleInput();
+  void update(float dt);
+  void Draw(float dt);
+};
+
+}  // namespace graphics
+
+#endif
